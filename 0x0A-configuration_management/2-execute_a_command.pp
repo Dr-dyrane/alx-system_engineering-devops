e2d8cbcd5e6059edc8ee1230e2b9eb5360ee1f32
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-25 13:55:40 +0100

    dyrane is awesome

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -14,8 +14,7 @@
 #                     /0x0A-configuration_management
 
 # Use the exec resource to run the pkill command to terminate the process
-exec { 'kill':
+exec { 'killmenow':
   command     => 'pkill -f killmenow',
   path        => ['/usr/bin', '/usr/sbin'],
-  refreshonly => true,
 }
