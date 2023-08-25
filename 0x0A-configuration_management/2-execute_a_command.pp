153dea41d29d887fd8017eb42841a9613169caad
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-25 13:28:40 +0100

    dyrane is awesome

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,23 +1,22 @@
 # Puppet Manifest: Execute Command to Kill Process
-
+#
 # This Puppet manifest executes a command to kill a process named "killmenow."
-
+#
 # Requirements:
 # - Must use the exec Puppet resource
 # - Must use pkill
-
+#
 # Example Usage:
 #   puppet apply 2-execute_a_command.pp
-
+#
 # Author: Alexander Udeogaranya
 # Repository: https://github.com/your/repo
 
 # Use the exec resource to run the pkill command to terminate the process
 exec { 'killmenow':
-  command     => 'pkill killmenow',
+  command     => '/usr/bin/pkill killmenow', # Specify the full path to pkill
   path        => '/usr/bin:/bin',
   refreshonly => true,
   logoutput   => true,
-  subscribe   => File['/path/to/killmenow/script'], # Replace with the actual path
-  onlyif      => 'pgrep killmenow',               # Check if the process is running
+  onlyif      => '/usr/bin/pgrep killmenow',  # Specify the full path to pgrep
 }
