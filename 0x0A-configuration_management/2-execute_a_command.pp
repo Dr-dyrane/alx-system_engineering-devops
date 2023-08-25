d4907dc07c2bacf052dafb79f1ff991165b6c0b1
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-08-25 13:50:52 +0100

    dyrane is awesome

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -14,10 +14,7 @@
 #                     /0x0A-configuration_management
 
 # Use the exec resource to run the pkill command to terminate the process
-exec { 'killmenow':
-  command     => '/usr/bin/pkill killmenow', # Specify the full path to pkill
-  path        => '/usr/bin:/bin',
-  refreshonly => true,
-  logoutput   => true,
-  onlyif      => '/usr/bin/pgrep killmenow',  # Specify the full path to pgrep
+exec { 'kill':
+  command     => 'pkill -f killmenow',
+  path        => ['/usr/bin', '/usr/sbin']
 }
