506df045dc2d13772e0ba343a9ea5b232622f8c0
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-10-10 19:04:35 +0100

    pep8

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" raddit api"""
+"""Reddit API"""
 
 import requests
 
@@ -16,7 +16,7 @@ def count_words(subreddit, word_list, after="", count=[]):
     if after == "":
         count = [0] * len(word_list)
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json".format(subreddit)
     request = requests.get(url,
                            params={'after': after},
                            allow_redirects=False,
@@ -25,35 +25,50 @@ def count_words(subreddit, word_list, after="", count=[]):
     if request.status_code == 200:
         data = request.json()
 
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
+        # Count words in each article
+        count_words_in_articles(data['data']['children'], word_list, count)
 
         after = data['data']['after']
         if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
-
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
-
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
+            # Combine duplicates and print results
+            combined_counts = combine_counts(word_list, count)
+            sorted_counts = sorted(
+                combined_counts.items(), key=lambda x: (-x[1], x[0].lower()))
+            for word, count in sorted_counts:
+                print(f"{word.lower()}: {count}")
         else:
             count_words(subreddit, word_list, after, count)
+
+
+def combine_counts(word_list, count):
+    """Combine duplicate words and lowercase them"""
+    combined_counts = {}
+    for i, word in enumerate(word_list):
+        word_lower = word.lower()
+        combined_counts[word_lower] = (
+            combined_counts.get(word_lower, 0) + count[i])
+    return combined_counts
+
+
+def count_words_in_articles(articles, word_list, count):
+    """Count words in articles"""
+    for article in articles:
+        title_words = article['data']['title'].split()
+        for word in title_words:
+            word_lower = word.lower()
+            for i, target_word in enumerate(word_list):
+                if target_word.lower() == word_lower:
+                    count[i] += 1
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print(
+            "Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+    else:
+        subreddit = sys.argv[1]
+        word_list = sys.argv[2].split()
+        count_words(subreddit, word_list)
