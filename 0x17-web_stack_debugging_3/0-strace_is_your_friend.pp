147167318db98cca52b8634ed66f4792d3bed5aa
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-10-11 17:31:21 +0100

    modified:   0x17-web_stack_debugging_3/0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,24 +1,10 @@
 # Puppet Manifest: 0-strace_is_your_friend.pp
 # Author: Alexander Udeogaranya
-# Description: Puppet manifest to automate the fix for Apache 500 error
+# Description: Puppet manifest to replace a line in a file on a server
 
-# Define the exec resource to run the fix
-exec { 'fix-wordpress':
-  command     => '/usr/sbin/fix_apache.sh', # The script that fixes the issue
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin', # Set the PATH environment variable
-  refreshonly => true, # Only run when notified
-  subscribe   => File['/usr/sbin/fix_apache.sh'], # Subscribe to changes in the fix script
-  logoutput   => true, # Log the output of the script
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
-
-# Define a file resource for the fix script
-file { '/usr/sbin/fix_apache.sh':
-  ensure  => present, # Ensure the file exists
-  source  => 'puppet:///modules/wordpress/fix_apache.sh', # Source of the script (you may need to provide the actual source path)
-  owner   => 'root', # Set the owner
-  group   => 'root', # Set the group
-  mode    => '0755', # Set the file permissions
-}
-
-# Notify the exec resource to run when the script is updated
-File['/usr/sbin/fix_apache.sh'] -> Exec['fix-wordpress']
