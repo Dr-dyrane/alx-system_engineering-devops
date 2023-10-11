4422b2e9801b6c260a5845667752f22c9a019129
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-10-11 17:35:05 +0100

    modified:   0x17-web_stack_debugging_3/0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,9 +2,11 @@
 # Author: Alexander Udeogaranya
 # Description: Puppet manifest to replace a line in a file on a server
 
+# Define the file path to the target file that needs the line replacement
 $file_to_edit = '/var/www/html/wp-settings.php'
 
+# Define an exec resource to execute the 'sed' command for line replacement
 exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",  # The 'sed' command to perform the line replacement
+  path    => ['/bin','/usr/bin']  # Set the command's search path
 }
