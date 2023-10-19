8b7fc68fbdb6599a21d7424774381940cc36fdd5
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-10-19 17:28:24 +0100

    bye devops

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,26 +1,22 @@
 # Puppet Manifest to Optimize Nginx for Handling High Traffic
 
 # Description:
-# This manifest increases the capacity of the Nginx web server
-# to handle a higher volume of concurrent connections.
+# This manifest aims to optimize the Nginx web server configuration to handle high traffic loads efficiently and reduce the number of failed requests.
 
 # Puppet Lint:
-# This manifest has been written to comply with
-# Puppet Lint version 2.1.1 without errors.
+# This manifest adheres to Puppet Lint version 2.1.1 without errors.
 
 # Puppet Version:
 # This manifest is intended for use with Puppet v3.4.
 
-# Ensure that all files have a new line at the end.
-file { '/etc/default/nginx':
-  ensure  => present,
-  content => template('module/nginx_config.erb'),
-  require => Class['nginx'],
+# Increase ULIMIT of the default file to improve Nginx performance
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 # Restart Nginx after updating the ulimit
-exec { 'nginx-restart':
-  command     => 'service nginx restart',
-  path        => '/etc/init.d/',
-  refreshonly => true,  # Only run when the 'file' resource above changes
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,22 @@
-# This manuscript enables the user holberton to login and open files without error
+# Puppet Manifest to Adjust User Limits for the holberton User
 
-# Increase hard file limit for user holberton
+# Description:
+# This manifest changes the OS configuration to enable the user 'holberton' to log in and open files without encountering "Too many open files" errors.
+
+# Puppet Lint:
+# This manifest adheres to Puppet Lint version 2.1.1 without errors.
+
+# Puppet Version:
+# This manifest is intended for use with Puppet v3.4.
+
+# Increase hard file limit for the user 'holberton'
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increase soft file limit for user holberton
+# Increase soft file limit for the user 'holberton'
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
-
